c9c01190dc595db3b4fbdd79155423e1d590c8e6
Antoine Petetin <antoine.petetin@gmail.com>
2018-12-27 10:58:12 +0100

    add unit tests for email verification

--- easylogin/src/main/java/antoinepetetin/fr/easylogin/EasyUserVerification.kt
+++ easylogin/src/main/java/antoinepetetin/fr/easylogin/EasyUserVerification.kt
@@ -1,12 +1,22 @@
 package antoinepetetin.fr.easylogin
 
 import android.util.Patterns
+import java.util.regex.Pattern
 
 abstract class EasyUserVerification {
     companion object {
+        val EMAIL_PATTERN = "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+"
+
         fun checkEmail(email: String?): Boolean{
-            return email.orEmpty().isNotEmpty() &&
-                    Patterns.EMAIL_ADDRESS.matcher(email!!.trim()).matches();
+            Patterns.EMAIL_ADDRESS
+            return !email.isNullOrBlank() &&
+                    Pattern.compile(EMAIL_PATTERN).matcher(email).matches()
         }
 
     }
